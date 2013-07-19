b935a8d6ecfc584f269b7ce5fbf090d027b6a342
Alex Papadimoulis <apapadimoulis@inedo.com>
2013-07-19 16:38:07 -0400

    added Dummy Agent

---
+++ DummyAgent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inedo.BuildMaster.Extensibility.Agents;
+using Inedo.BuildMaster.Data;
+
+namespace Inedo.BuildMasterExtensions.Dummy
+{
+    [AgentProperties("Dummy Agent",
+        "An agent for testing purposes")]
+    public class DummyAgent : AgentBase
+    {
+        protected override TService GetAgentServiceInternal<TService>()
+        {
+            return this as TService;
+        }
+
+        protected override string GetAgentStatusInternal(IHostedAgentContext context)
+        {
+            return Domains.ServerStatus.Ready;
+        }
+    }
+}
